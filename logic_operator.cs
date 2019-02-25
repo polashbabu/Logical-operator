f6125d8203e1a2c6b0dd848773a4e2e8fa217167
MD.Jahid Hassan <32840051+polashbabu@users.noreply.github.com>
2019-02-25 12:27:45 +0600

    Add files via upload

---
+++ logic_operator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Logical_Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your first number");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input your second number ");
+            int number2 = int.Parse(Console.ReadLine());
+            if (number1 > number2)
+            {
+                Console.WriteLine("First number is grather then second number ");
+
+            }
+            else if (number1==number2)
+            {
+                Console.WriteLine("first and second number is same ");
+            }
+
+            else
+            {
+                Console.WriteLine("first number is less then second number ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
